bebcf157229383f8f8d22c73b04d24025eb0cb07
QDoutriaux <quentin.doutriaux.partner@decathlon.com>
2024-04-16 21:25:59 +0200

    feat(ui): Add text to generic loading

--- lib/components/generics/generic_loading.dart
+++ lib/components/generics/generic_loading.dart
@@ -1,12 +1,25 @@
 import "package:flutter/material.dart";
+import "package:footrack_front/core/ui/spacings.dart";
 
 class GenericLoading extends StatelessWidget {
-  const GenericLoading({super.key});
+  const GenericLoading({
+    super.key,
+    this.text = "",
+  });
+
+  final String text;
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(),
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const CircularProgressIndicator(),
+          const SizedBox(width: Spacing.xs),
+          Text(text),
+        ],
+      ),
     );
   }
 }
