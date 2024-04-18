e5b1a972b97c04a27acb2849a905f181081c0122
QDoutriaux <quentin.doutriaux.partner@decathlon.com>
2024-04-18 18:21:38 +0200

    fix(seasons): Fix seasons datatable scroll

--- lib/pages/seasons_list/components/seasons_datatable.dart
+++ lib/pages/seasons_list/components/seasons_datatable.dart
@@ -18,99 +18,101 @@ class SeasonsDatatable extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return DataTable(
-      showCheckboxColumn: false,
-      headingRowHeight: 35,
-      headingTextStyle: const TextStyle(
-        color: Colors.black,
-        fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: DataTable(
+        showCheckboxColumn: false,
+        headingRowHeight: 35,
+        headingTextStyle: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        columnSpacing: 8,
+        columns: [
+          DataColumn(
+            label: Text(
+              "Saison (${seasons.length})",
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+          ),
+          DataColumn(
+            label: Text(
+              "Matchs",
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            numeric: true,
+          ),
+          DataColumn(
+            label: Text(
+              "BP",
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            numeric: true,
+          ),
+          DataColumn(
+            label: Text(
+              "BC",
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            numeric: true,
+          ),
+        ],
+        rows: List.of(seasons).map((season) {
+          return DataRow(
+            cells: [
+              DataCell(
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      season.getName(),
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      maxLines: 1,
+                    ),
+                    Text(
+                      "${season.getFrom().format()}${season.getTo() != null ? " - " : ""}${season.getTo().format()}",
+                      style: Theme.of(context).textTheme.labelMedium,
+                    ),
+                  ],
+                ),
+              ),
+              DataCell(
+                Text(
+                  "${season.nbPlayedMatchs(ref)}/${season.nbMatches(ref)}",
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              DataCell(
+                Text(
+                  season.nbGoalsFor(ref).toString(),
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              DataCell(
+                Text(
+                  season.nbGoalsAgainst(ref).toString(),
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+            ],
+            onSelectChanged: (selected) {
+              onSeasonClick?.call(season);
+            },
+            onLongPress: () {
+              onSeasonLongClick?.call(season);
+            },
+          );
+        }).toList(),
       ),
-      columnSpacing: 8,
-      columns: [
-        DataColumn(
-          label: Text(
-            "Saison (${seasons.length})",
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-        ),
-        DataColumn(
-          label: Text(
-            "Matchs",
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-          numeric: true,
-        ),
-        DataColumn(
-          label: Text(
-            "BP",
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-          numeric: true,
-        ),
-        DataColumn(
-          label: Text(
-            "BC",
-            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-          ),
-          numeric: true,
-        ),
-      ],
-      rows: List.of(seasons).map((season) {
-        return DataRow(
-          cells: [
-            DataCell(
-              Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    season.getName(),
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    maxLines: 1,
-                  ),
-                  Text(
-                    "${season.getFrom().format()}${season.getTo() != null ? " - " : ""}${season.getTo().format()}",
-                    style: Theme.of(context).textTheme.labelMedium,
-                  ),
-                ],
-              ),
-            ),
-            DataCell(
-              Text(
-                "${season.nbPlayedMatchs(ref)}/${season.nbMatches(ref)}",
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ),
-            DataCell(
-              Text(
-                season.nbGoalsFor(ref).toString(),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ),
-            DataCell(
-              Text(
-                season.nbGoalsAgainst(ref).toString(),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-            ),
-          ],
-          onSelectChanged: (selected) {
-            onSeasonClick?.call(season);
-          },
-          onLongPress: () {
-            onSeasonLongClick?.call(season);
-          },
-        );
-      }).toList(),
     );
   }
 }
